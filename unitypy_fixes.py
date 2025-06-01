85808f67e41ce7240ab96401194b4bd2494b1165
Matthias Balota <balotamatthias@gmail.com>
2025-06-01 12:28:30 +0200

    Updates UnityPy

--- unitypy_fixes.py
+++ unitypy_fixes.py
@@ -3,36 +3,29 @@ import warnings
 
 from UnityPy.enums import TextureFormat
 
+TF = TextureFormat
 
 def compress_etcpak(
     data: bytes, width: int, height: int, target_texture_format: TextureFormat
 ) -> bytes:
     import etcpak  # etcpak is imported locally in the original function
 
-    if target_texture_format in [TextureFormat.DXT1, TextureFormat.DXT1Crunched]:
+    if target_texture_format in [TF.DXT1, TF.DXT1Crunched]:
         return etcpak.compress_bc1(data, width, height)
-    elif target_texture_format in [TextureFormat.DXT5, TextureFormat.DXT5Crunched]:
+    elif target_texture_format in [TF.DXT5, TF.DXT5Crunched]:
         return etcpak.compress_bc3(data, width, height)
-    elif target_texture_format == TextureFormat.BC4:
+    elif target_texture_format == TF.BC4:
         return etcpak.compress_bc4(data, width, height)
-    elif target_texture_format == TextureFormat.BC5:
+    elif target_texture_format == TF.BC5:
         return etcpak.compress_bc5(data, width, height)
-    elif target_texture_format == TextureFormat.BC7:
+    elif target_texture_format == TF.BC7:
         # Modified call to compress_bc7, not passing 'None' as the second argument as it does not accept it
         return etcpak.compress_bc7(data, width, height)
-    elif target_texture_format in [
-        TextureFormat.ETC_RGB4,
-        TextureFormat.ETC_RGB4Crunched,
-        TextureFormat.ETC_RGB4_3DS,
-    ]:
+    elif target_texture_format in [TF.ETC_RGB4, TF.ETC_RGB4Crunched, TF.ETC_RGB4_3DS]:
         return etcpak.compress_etc1_rgb(data, width, height)
-    elif target_texture_format == TextureFormat.ETC2_RGB:
+    elif target_texture_format == TF.ETC2_RGB:
         return etcpak.compress_etc2_rgb(data, width, height)
-    elif target_texture_format in [
-        TextureFormat.ETC2_RGBA8,
-        TextureFormat.ETC2_RGBA8Crunched,
-        TextureFormat.ETC2_RGBA1,
-    ]:
+    elif target_texture_format in [TF.ETC2_RGBA8, TF.ETC2_RGBA8Crunched, TF.ETC2_RGBA1]:
         return etcpak.compress_etc2_rgba(data, width, height)
     else:
         raise NotImplementedError(
